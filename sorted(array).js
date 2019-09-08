f8ee51519fb97fff3ee08274d6e356e5b6846f6b
corps01 <50376088+corps01@users.noreply.github.com>
2019-09-07 21:16:49 -0500

    Add files via upload

---
+++ sorted(array).js
@@ -0,0 +1,15 @@
+const sort = arr => {
+  for (let i = 0; i < arr.length; i++){
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[i] < arr[j]) {
+            let tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        
+            }
+    }
+  }    
+  return arr;
+}
+
+console.log(sort([1,8,23,-1,96,43,2,15]))
